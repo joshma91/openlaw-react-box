8afb42efa312c1716b31a075499a21e78300092c
Joshma91 <josh.ma91@gmail.com>
2018-12-16 16:33:46 -0800

    correct mnemonic spelling

--- truffle.js
+++ truffle.js
@@ -1,51 +1,43 @@
 const HDWalletProvider = require("truffle-hdwallet-provider");
-require('dotenv').config()
 
-// See <http://truffleframework.com/docs/advanced/configuration>
-// to customize your Truffle configuration!
+require('dotenv').config()  // Store environment-specific variable from '.env' to process.env
+
 module.exports = {
   networks: {
     development: {
       host: 'localhost',
       port: 8545,
       network_id: '*' // Match any network id
-    }, 
+    },
+    // testnets
+    // properties
+    // network_id: identifier for network based on ethereum blockchain. Find out more at https://github.com/ethereumbook/ethereumbook/issues/110
+    // gas: gas limit
+    // gasPrice: gas price in gwei
     ropsten: {
-      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
       network_id: 3,
       gas: 3000000,
       gasPrice: 21
     },
     kovan: {
-      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
       network_id: 42,
       gas: 3000000,
       gasPrice: 21
     },
     rinkeby: {
-      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
       network_id: 4,
-      gas: 3000000,
-      gasPrice: 21
+      gas: 4000000,
+      gasPrice: 30
     },
     // main ethereum network(mainnet)
     main: {
-      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
       network_id: 1,
       gas: 3000000,
       gasPrice: 21
-    }
-  },
-  solc: {
-    // Turns on the Solidity optimizer. For development the optimizer's
-    // quite helpful, just remember to be careful, and potentially turn it
-    // off, for live deployment and/or audit time. For more information,
-    // see the Truffle 4.0.0 release notes.
-    //
-    // https://github.com/trufflesuite/truffle/releases/tag/v4.0.0
-    optimizer: {
-      enabled: true,
-      runs: 200
     }
   }
 }
